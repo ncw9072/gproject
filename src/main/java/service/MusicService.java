ea28facebf8fdfa582c282c41d400f5672e5f246
ncw9072 <71861771+ncw9072@users.noreply.github.com>
2021-04-13 00:19:56 +0900

    0413

---
+++ src/main/java/service/MusicService.java
@@ -0,0 +1,15 @@
+package service;
+
+import java.util.List;
+
+import vo.MusicVO;
+
+public interface MusicService {
+
+	public List<MusicVO> selectList();
+	public MusicVO selectOne(MusicVO vo);
+	public void musicCount(MusicVO vo);
+	public List<MusicVO> selectdailyRank();
+	
+	
+}// interface
